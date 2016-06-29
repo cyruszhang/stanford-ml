0551f3a33da8dcddd67d4850a03054d06d5801af
Cryusz Zhang <cyrusz@microsoft.com>
2016-06-29 15:02:16 -0700

    finish ex7

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -25,13 +25,17 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
-
-
+for i=1:K
+  centroid = zeros(1, n);
+  num = 0;
+  for j=1:m
+    if (idx(j) == i)
+      centroid += X(j, :); 
+      num ++;
+    endif
+  end
+  centroids(i, :) = centroid/num;
+end
 
 % =============================================================
 
