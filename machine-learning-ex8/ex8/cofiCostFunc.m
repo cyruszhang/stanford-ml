ff57d5b811de8a942f24f6d693079bfeb5f27a55
Cryusz Zhang <cyrusz@microsoft.com>
2016-07-01 15:13:27 -0700

    finish ex8_cofi

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,20 +40,21 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J =  sum(((X * Theta' - Y).^2.*R)(:))/2 + sum(Theta(:).^2)*lambda/2 + sum(X(:).^2)*lambda/2;
 
+for i=1:num_movies
+  idx = find(R(i,:) == 1);
+  Theta_tmp = Theta(idx, :);
+  Y_tmp = Y(i, idx);
+  X_grad(i,:) = (X(i,:) * Theta_tmp' - Y_tmp) * Theta_tmp + lambda*X(i,:);
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
+for j=1:num_users
+  idx = find(R(:,j) == 1);
+  X_tmp = X(idx, :);
+  Y_tmp = Y(idx, j);
+  Theta_grad(j,:) = (X_tmp * Theta(j,:)' - Y_tmp)' * X_tmp + lambda*Theta(j,:);
+end
 
 % =============================================================
 
