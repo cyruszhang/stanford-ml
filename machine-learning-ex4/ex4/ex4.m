e29e7c8805dfc04aca8e0229a7b0f3204874dbcb
Cryusz Zhang <cyrusz@microsoft.com>
2016-09-21 16:42:03 -0700

    add 2 hidden layers

--- machine-learning-ex4/ex4/ex4.m
+++ machine-learning-ex4/ex4/ex4.m
@@ -45,79 +45,6 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 
-%% ================ Part 2: Loading Parameters ================
-% In this part of the exercise, we load some pre-initialized 
-% neural network parameters.
-
-fprintf('\nLoading Saved Neural Network Parameters ...\n')
-
-% Load the weights into variables Theta1 and Theta2
-load('ex4weights.mat');
-
-% Unroll parameters 
-nn_params = [Theta1(:) ; Theta2(:)];
-
-%% ================ Part 3: Compute Cost (Feedforward) ================
-%  To the neural network, you should first start by implementing the
-%  feedforward part of the neural network that returns the cost only. You
-%  should complete the code in nnCostFunction.m to return cost. After
-%  implementing the feedforward to compute the cost, you can verify that
-%  your implementation is correct by verifying that you get the same cost
-%  as us for the fixed debugging parameters.
-%
-%  We suggest implementing the feedforward cost *without* regularization
-%  first so that it will be easier for you to debug. Later, in part 4, you
-%  will get to implement the regularized cost.
-%
-fprintf('\nFeedforward Using Neural Network ...\n')
-
-% Weight regularization parameter (we set this to 0 here).
-lambda = 0;
-
-J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
-                   num_labels, X, y, lambda);
-
-fprintf(['Cost at parameters (loaded from ex4weights): %f '...
-         '\n(this value should be about 0.287629)\n'], J);
-
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
-
-%% =============== Part 4: Implement Regularization ===============
-%  Once your cost function implementation is correct, you should now
-%  continue to implement the regularization with the cost.
-%
-
-fprintf('\nChecking Cost Function (w/ Regularization) ... \n')
-
-% Weight regularization parameter (we set this to 1 here).
-lambda = 1;
-
-J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
-                   num_labels, X, y, lambda);
-
-fprintf(['Cost at parameters (loaded from ex4weights): %f '...
-         '\n(this value should be about 0.383770)\n'], J);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-
-%% ================ Part 5: Sigmoid Gradient  ================
-%  Before you start implementing the neural network, you will first
-%  implement the gradient for the sigmoid function. You should complete the
-%  code in the sigmoidGradient.m file.
-%
-
-fprintf('\nEvaluating sigmoid gradient...\n')
-
-g = sigmoidGradient([1 -0.5 0 0.5 1]);
-fprintf('Sigmoid gradient evaluated at [1 -0.5 0 0.5 1]:\n  ');
-fprintf('%f ', g);
-fprintf('\n\n');
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
 
 
 %% ================ Part 6: Initializing Pameters ================
@@ -135,43 +62,6 @@ initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
 initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
 
 
-%% =============== Part 7: Implement Backpropagation ===============
-%  Once your cost matches up with ours, you should proceed to implement the
-%  backpropagation algorithm for the neural network. You should add to the
-%  code you've written in nnCostFunction.m to return the partial
-%  derivatives of the parameters.
-%
-fprintf('\nChecking Backpropagation... \n');
-
-%  Check gradients by running checkNNGradients
-checkNNGradients;
-
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
-
-
-%% =============== Part 8: Implement Regularization ===============
-%  Once your backpropagation implementation is correct, you should now
-%  continue to implement the regularization with the cost and gradient.
-%
-
-fprintf('\nChecking Backpropagation (w/ Regularization) ... \n')
-
-%  Check gradients by running checkNNGradients
-lambda = 3;
-checkNNGradients(lambda);
-
-% Also output the costFunction debugging values
-debug_J  = nnCostFunction(initial_nn_params, input_layer_size, ...
-                          hidden_layer_size, num_labels, X, y, lambda);
-
-fprintf(['\n\nCost at (fixed) debugging parameters (w/ lambda = 10): %f ' ...
-         '\n(this value should be about 0.576051)\n\n'], debug_J);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-
 %% =================== Part 8: Training NN ===================
 %  You have now implemented all the code necessary to train a neural 
 %  network. To train your neural network, we will now use "fmincg", which
@@ -186,7 +76,7 @@ fprintf('\nTraining Neural Network... \n')
 options = optimset('MaxIter', 50);
 
 %  You should also try different values of lambda
-lambda = 1;
+lambda = 5;
 
 % Create "short hand" for the cost function to be minimized
 costFunction = @(p) nnCostFunction(p, ...
